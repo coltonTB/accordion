75a6ccaa28bbc0a3af1043f75867436054770282
colton brown <colton@joinhoney.com>
2015-03-17 16:59:42 -0700

    added handlebars support, refactored

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp       = require('gulp'),
     stylus     = require('gulp-stylus'),
-    rename     = require('gulp-rename');
+    rename     = require('gulp-rename'),
+    handlebars = require('gulp-handlebars'),
+    wrap       = require('gulp-wrap'),
+    declare    = require('gulp-declare'),
+    concat     = require('gulp-concat');
 
 gulp.task('stylus', function () {
   return gulp.src('./stylesheets/index.styl')
@@ -9,9 +13,24 @@ gulp.task('stylus', function () {
     .pipe(gulp.dest('.'))
 });
 
+gulp.task('templates', function() {
+  gulp.src('./templates/*.hbs')
+    .pipe(handlebars())
+    .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, Handlebars.template(<%= contents %>));', {}, {
+      imports: {
+        processPartialName: function(fileName) {
+          return JSON.stringify(fileName.split('.')[0]);
+        }
+      }
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('.'));
+});
+
 gulp.task('watch', function(){
   gulp.watch('./stylesheets/*.styl', ['stylus']);
+  gulp.watch('./templates/*.hbs', ['templates']);
 });
 
 
-gulp.task('default', ['stylus', 'watch']);
+gulp.task('default', ['stylus', 'templates', 'watch']);
